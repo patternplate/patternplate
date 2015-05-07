f4efe2d3d2e6cb89570d04fa6288ea89d87c1f2f
Mario Nebl <mario.nebl@sinnerschrader.com>
2015-05-07 15:07:26 +0200

    Prepare for easier setup of patternplate-projects

---
+++ source/binary/_patternplate.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+/*eslint-disable no-process-env */
+import {resolve} from 'path';
+
+import patternPlateServer from 'patternplate-server';
+import patternPlateClient from 'patternplate-client';
+
+export default async function start (options = {}) {
+	let server = await patternPlateServer({
+		'server': {
+			'port': 1338
+		},
+		'cwd': options.cwd || resolve(require.resolve('patternplate-server'), '..', '..'),
+		'patterncwd': options.patterncwd || process.cwd()
+	});
+
+	await patternPlateClient({
+		'env': 'production',
+		'cwd': resolve(require.resolve('patternplate-client'), '..', '..')
+	});
+
+	return server;
+}
---
+++ source/binary/patternplate.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+/*eslint-disable no-sync */
+require('babel/polyfill');
+var minimist = require('minimist');
+
+var start = require('./_patternplate');
+var args = minimist(process.argv.slice(1));
+
+start(args)
+	.then(function startCompleted (application) {
+		application.log.info('[application]', 'Started server ...');
+	})
+	.catch(function startFailed (err, application) {
+		var log = application ? application.log || console : console;
+		log.trace(err);
+	});
