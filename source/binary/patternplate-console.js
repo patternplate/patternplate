d681d0a31da648ef0644cfd8cf3ec2cc503fef89
Mario Nebl <mario.nebl@sinnerschrader.com>
2015-09-24 17:29:05 +0200

    Update to new application.run api, 0.14.0

--- source/binary/patternplate-console.js
+++ source/binary/patternplate-console.js
@@ -6,24 +6,16 @@ import minimist from 'minimist';
 
 import patternplate from '../';
 
-async function start ( options = {} ) {
-	let application;
-	let settings = Object.assign( options, { 'mode': 'console' } );
+async function start (options) {
+	const mode = 'console';
+	const settings = {...options, mode};
+	const command = settings._[1];
 
-	try {
-		application = await patternplate( settings );
-	} catch ( error ) {
-		let log = application ? application.log || console : console;
-		log.error( error );
-		throw new Error( error );
-	}
-
-	try {
-		await application.server.run( settings );
-	} catch ( error ) {
-		application.log.error( error );
-		throw new Error( error );
-	}
+	const application = await patternplate(settings);
+	await application.server.run(command, settings);
 }
 
-start( minimist( process.argv.slice( 1 ) ) );
+const args = minimist(process.argv.slice(1));
+
+start(args)
+	.catch(err => { throw err; });
