2d5dc2278cbe1595a10e7e51e9c9c2f2e01d398d
Mario Nebl <hello@herebecode.com>
2016-02-15 20:42:40 +0100

    fix: correct react version detection

--- @patternplate/patternplate-transform-react-to-markup/source/index.js
+++ @patternplate/patternplate-transform-react-to-markup/source/index.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import compareSemver from 'compare-semver';
 import {merge} from 'lodash';
 
 function getRenderFunction(toStatic = true) {
-	const nameSpace = React.version < 0.14 ?
+	const hasReactDOM = compareSemver.gt(React.version, ['0.13.3']);
+	const nameSpace = hasReactDOM ?
 		React :
 		require('react-dom/server');
 	return toStatic ?
