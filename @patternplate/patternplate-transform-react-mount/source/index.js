dd7c6d238d47bc8badb39e871479459927e8a9c6
Mario Nebl <hello@herebecode.com>
2016-02-18 14:46:26 +0100

    fix: enable react version interoperability

--- @patternplate/patternplate-transform-react-mount/source/index.js
+++ @patternplate/patternplate-transform-react-mount/source/index.js
@@ -1,14 +1,24 @@
 import * as React from 'react';
+import {yellow} from 'chalk';
 import compareSemver from 'compare-semver';
+import {resolve} from 'try-require';
 
-const hasReactDOM = compareSemver.gt(React.version, ['0.13.3']);
+const deprecation = `[ ⚠ External Deprecation ⚠ ]`;
 
-export default function () {
+export default function (application) {
 	return async file => {
-		const requireStatement = hasReactDOM ?
+		const wantsReactDOM = compareSemver.gt(React.version, ['0.13.3']);
+		const reactDOMavailable = typeof resolve('react-dom') !== 'undefined';
+
+		if (wantsReactDOM && !reactDOMavailable) {
+			const warning = `${deprecation} React version ${React.version} deprecated React.render and moved it to react-dom's render, but react-dom is not available via require.resolve. Consider installing react-dom.`;
+			application.log.warn(yellow(warning));
+		}
+
+		const requireStatement = wantsReactDOM && reactDOMavailable ?
 			`var ReactDOM = require('react-dom')` : '';
 
-		const renderStatement = hasReactDOM ?
+		const renderStatement = wantsReactDOM && reactDOMavailable ?
 			'ReactDOM.render(mountableElement, mountElement);' :
 			'React.render(mountableElement, mountElement);';
 
