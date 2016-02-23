b83c2a4793d3ff8ff32b2bb1ca93b6f839ce3d03
Mario Nebl <hello@herebecode.com>
2016-02-23 18:57:58 +0100

    fix: provide console to code evaluated
    * remove unneeded __dirname __filename globals
    * add console global to jsx sandbox
    * give hint about phase the error occurs in
    
    * https://github.com/sinnerschrader/patternplate-transform-react-to-markup/issues/1
    * https://github.com/sinnerschrader/patternplate-server/issues/54

--- @patternplate/patternplate-transform-react-to-markup/source/get-component.js
+++ @patternplate/patternplate-transform-react-to-markup/source/get-component.js
@@ -3,24 +3,45 @@ import {
 	Script
 } from 'vm';
 
-export default file => {
+import chalk from 'chalk';
+
+export default (file, application) => {
 	const source = file.buffer.toString('utf-8');
+	const prefix = chalk.grey(`[${[file.pattern.id, file.name].join(':')}]`);
 
 	// prepare script and context
 	const sandbox = {
 		module,
 		exports,
 		require,
-		__filename,
-		__dirname
+		// Provide proxied console, prepend with file
+		console: {
+			log(...args) {
+				application.log.info(...[prefix, ...args]);
+			},
+			debug(...args) {
+				application.log.debug(...[prefix, ...args]);
+			},
+			error(...args) {
+				application.log.error(...[prefix, ...args]);
+			},
+			info(...args) {
+				application.log.info(...[prefix, ...args]);
+			},
+			warn(...args) {
+				application.log.warn(...[prefix, ...args]);
+			},
+			trace(...args) {
+				application.log.trace(...[prefix, ...args]);
+			}
+		}
 	};
 
 	sandbox.global = sandbox;
-
 	const context = new Context(sandbox);
 	const script = new Script(source);
 
-	// execute code
+	// execute module
 	try {
 		script.runInContext(context, {
 			filename: file.path,
@@ -30,11 +51,10 @@ export default file => {
 			timeout: 5000
 		});
 	} catch (error) {
-		const message = `Error ${error.lineNumber} in file ${file.path} of pattern ${file.pattern.id}:`;
+		const message = `Error during parsing of file ${file.path} in pattern ${file.pattern.id}:`;
 		error.message = [
 			message,
-			error.message,
-			source
+			error.message
 		].join('\n');
 		throw error;
 	}
