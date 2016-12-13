cc50739154bce97c3c0283f88991811b1abd4cb9
Mario Nebl <hello@herebecode.com>
2016-12-13 07:31:31 +0100

    fix: produce pages for folders
    closes #134

--- source/application/tasks/build-interface/build-pages.js
+++ source/application/tasks/build-interface/build-pages.js
@@ -14,7 +14,18 @@ function buildPages(ids, target, context) {
 		const {app, rewriter} = context;
 		const idPad = padEnd(max(ids.map(id => id.id.length)));
 
-		build(ids, {
+		const pages = ids.reduce((pages, page) => {
+			page.relative.forEach((_, index) => {
+				const relative = page.relative.slice(0, index + 1);
+				const id = relative.join('/');
+				pages[id] = {id, name: `${id}/index.html`, relative: []};
+			});
+
+			pages[page.id] = page;
+			return pages;
+		}, {});
+
+		build(pages, {
 			async read(id, ids, count) {
 				observer.next(`${idPad(id.id)} ${count}/${ids.length}`);
 				return renderPage(app, `/pattern/${id.id}`);
