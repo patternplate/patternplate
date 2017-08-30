c4d691021b211c16f31beae55628caa48241cfaf
mario.nebl@sinnerschrader.com <mario.nebl@sinnerschrader.com>
2017-08-30 16:58:53 +0200

    perf: speed things along via browserify.vendors

--- @patternplate/components/configuration/patternplate-server/transforms.js
+++ @patternplate/components/configuration/patternplate-server/transforms.js
@@ -1,24 +1,25 @@
 module.exports = {
-	'babel': {
-		inFormat: 'jsx',
-		outFormat: 'js',
-		opts: {
+  'babel': {
+    inFormat: 'jsx',
+    outFormat: 'js',
+    opts: {
       presets: ['env', 'react'],
       plugins: [
         'transform-object-rest-spread'
       ]
     }
-	},
-	'browserify': {
-		inFormat: 'js',
-		outFormat: 'js'
-	},
-	'react-mount': {
-		inFormat: 'js',
-		outFormat: 'js'
-	},
-	'react-to-markup': {
-		inFormat: 'js',
-		outFormat: 'html'
-	}
+  },
+  'browserify': {
+    inFormat: 'js',
+    outFormat: 'js',
+    vendors: ['react', 'react-dom', 'styled-components']
+  },
+  'react-mount': {
+    inFormat: 'js',
+    outFormat: 'js'
+  },
+  'react-to-markup': {
+    inFormat: 'js',
+    outFormat: 'html'
+  }
 };
