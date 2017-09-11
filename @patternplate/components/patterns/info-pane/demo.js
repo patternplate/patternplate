c2b6709d76dedda38af3a62abdc9ca068e2dd4fd
dangoo <daniel.gooss@sinnerschrader.com>
2017-09-11 18:02:39 +0200

    feat(components): Added info-pane

---
+++ @patternplate/components/patterns/info-pane/demo.js
@@ -0,0 +1,27 @@
+const React = require('react');
+const InfoPane = require('Pattern');
+const styled = require('styled-components').default;
+const Themer = require('../demo-themer');
+
+module.exports = function InfoPaneDemo() {
+  return (
+    <Themer>
+      <StyledDemoBox>
+        <InfoPane
+          standalone
+          hermit
+          icon="patternplate"
+          name="Test pane"
+          id="testpane"
+          version="1.0.0"
+          flag="beta"
+        />
+      </StyledDemoBox>
+    </Themer>
+  );
+}
+
+const StyledDemoBox = styled.div`{
+    margin: 10px 0 0 10px;
+  }
+`
