8b20f008bdab6f3dd855356d4217c627ac2bd668
mario.nebl@sinnerschrader.com <mario.nebl@sinnerschrader.com>
2017-09-13 17:39:34 +0200

    chore: tend gitignore

--- packages/client/application/actions/dismiss-all-messages.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-const _reduxActions = require('redux-actions');
-
-exports.default = (0, _reduxActions.createAction)('DISMISS_ALL_MESSAGES');
--- packages/client/application/actions/toggle-navigation.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-const _toggle = require('./toggle');
-
-exports.default = (0, _toggle.toggle)('navigation-enabled');
