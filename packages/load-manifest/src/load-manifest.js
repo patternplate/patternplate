01c5c16221bf9a66708f2101a18e6227c4beb737
Mario Nebl <hello@mario-nebl.de>
2018-03-02 14:05:39 +0100

    feat: throw for missing file

--- packages/load-manifest/src/load-manifest.js
+++ packages/load-manifest/src/load-manifest.js
@@ -1,9 +1,24 @@
+const sander = require("@marionebl/sander");
+
+const PATTERNPLATE_ERR_NO_MANIFEST = 'PATTERNPLATE_ERR_NO_MANIFEST';
+
 module.exports = {
+  PATTERNPLATE_ERR_NO_MANIFEST,
   loadManifest
 };
+
+const map = fn => Promise.all(['pattern.json', 'package.json'].map(fn));
 
 async function loadManifest(dir) {
   if (typeof dir !== "string") {
     throw new Error(`load-manifest dir expects string, received ${dir}, typeof ${dir}`);
+  }
+
+  const files = await map(f => sander.exists(dir, f));
+
+  if (!files.some(Boolean)) {
+    const err = new Error(`load-manifest could not find pattern.json, package.json in ${dir}`);
+    err.errno = PATTERNPLATE_ERR_NO_MANIFEST;
+    throw err;
   }
 }
--- packages/load-manifest/src/load-manifest.test.js
+++ packages/load-manifest/src/load-manifest.test.js
@@ -1,7 +1,20 @@
+const fixturez = require("fixturez");
+const {PATTERNPLATE_ERR_NO_MANIFEST} = require("./load-manifest");
 const {loadManifest} = require("./load-manifest");
+
+const f = fixturez(__dirname);
 
 test("throws for empty input", async () => {
   await expect(loadManifest()).rejects.toMatchObject({
     message: expect.stringContaining("expects string")
+  })
+});
+
+test("throws for missing file", async () => {
+  const cwd = f.copy("empty");
+
+  await expect(loadManifest(cwd)).rejects.toMatchObject({
+    message: expect.stringContaining("could not find"),
+    errno: PATTERNPLATE_ERR_NO_MANIFEST
   })
 });
