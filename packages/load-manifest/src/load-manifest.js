9f732bdd9be704d38c40e9b55f8f415793fe68c2
Mario Nebl <hello@mario-nebl.de>
2018-03-02 14:18:30 +0100

    feat: handle malformed json files

--- packages/load-manifest/src/load-manifest.js
+++ packages/load-manifest/src/load-manifest.js
@@ -1,24 +1,41 @@
+const path = require("path");
+const loadJsonFile = require("load-json-file");
 const sander = require("@marionebl/sander");
 
 const PATTERNPLATE_ERR_NO_MANIFEST = 'PATTERNPLATE_ERR_NO_MANIFEST';
+const PATTERNPLATE_ERR_MALFORMED_MANIFEST = 'PATTERNPLATE_ERR_MALFORMED_MANIFEST';
 
 module.exports = {
   PATTERNPLATE_ERR_NO_MANIFEST,
+  PATTERNPLATE_ERR_MALFORMED_MANIFEST,
   loadManifest
 };
 
-const map = fn => Promise.all(['pattern.json', 'package.json'].map(fn));
+const map = fn => Promise.all(['package.json', 'pattern.json'].map(fn));
 
 async function loadManifest(dir) {
   if (typeof dir !== "string") {
     throw new Error(`load-manifest dir expects string, received ${dir}, typeof ${dir}`);
   }
 
-  const files = await map(f => sander.exists(dir, f));
+  const files = (await map(async f => (await sander.exists(dir, f)) ? f : null)).filter(Boolean);
+  const file = files[0];
 
-  if (!files.some(Boolean)) {
+  if (!file) {
     const err = new Error(`load-manifest could not find pattern.json, package.json in ${dir}`);
     err.errno = PATTERNPLATE_ERR_NO_MANIFEST;
+    throw err;
+  }
+
+  const fullPath = path.resolve(dir, file);
+  const data = await loadJSON(fullPath);
+}
+
+async function loadJSON(file) {
+  try {
+    return await loadJsonFile(file);
+  } catch (err) {
+    err.errno = PATTERNPLATE_ERR_MALFORMED_MANIFEST;
     throw err;
   }
 }
--- packages/load-manifest/src/load-manifest.test.js
+++ packages/load-manifest/src/load-manifest.test.js
@@ -1,20 +1,29 @@
 const fixturez = require("fixturez");
-const {PATTERNPLATE_ERR_NO_MANIFEST} = require("./load-manifest");
+const errors = require("./load-manifest");
 const {loadManifest} = require("./load-manifest");
 
 const f = fixturez(__dirname);
 
-test("throws for empty input", async () => {
+test("throws for missing input", async () => {
   await expect(loadManifest()).rejects.toMatchObject({
     message: expect.stringContaining("expects string")
-  })
+  });
 });
 
 test("throws for missing file", async () => {
-  const cwd = f.copy("empty");
+  const cwd = f.copy("missing");
 
   await expect(loadManifest(cwd)).rejects.toMatchObject({
     message: expect.stringContaining("could not find"),
-    errno: PATTERNPLATE_ERR_NO_MANIFEST
-  })
+    errno: errors.PATTERNPLATE_ERR_NO_MANIFEST
+  });
+});
+
+test("throws for malformed file", async () => {
+  const cwd = f.copy("malformed");
+
+  await expect(loadManifest(cwd)).rejects.toMatchObject({
+    message: expect.stringContaining("Unexpected token"),
+    errno: errors.PATTERNPLATE_ERR_MALFORMED_MANIFEST
+  });
 });
