2ad8852563475bf422298644efee7409d06b22e7
Mario Nebl <hello@mario-nebl.de>
2018-03-05 13:00:42 +0100

    fix: ensure correct url for meta updates

--- packages/client/src/app/actions/load-schema.js
+++ packages/client/src/app/actions/load-schema.js
@@ -5,8 +5,13 @@ import fetch from "../utils/fetch";
 export default createPromiseThunkAction(
   "LOAD_SCHEMA",
   async (_, __, getState) => {
-    const uri = [getState().base, "api/state.json"].join("/");
-    const response = await fetch(uri);
+    const response = await fetch(`${prefix(base)}/api/state.json`);
     return response.json();
   }
 );
+
+function prefix(base) {
+  return base.charAt(base.length - 1) === "/"
+    ? base.slice(0, base.length - 1)
+    : base;
+}
