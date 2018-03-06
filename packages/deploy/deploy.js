1cc8abbfe21413b2958fabaee77971310d869510
Mario Nebl <hello@mario-nebl.de>
2018-03-06 19:57:12 +0100

    chore: set up deployment to GitHub pages

--- packages/deploy/deploy.js
+++ packages/deploy/deploy.js
@@ -1,26 +1,37 @@
 #!/usr/bin/env node
 const meow = require("meow");
 const execa = require("execa");
+const sander = require("@marionebl/sander");
 
-const git = (args, {cwd}) => {
-  execa("git", args, { cwd, stdout: "inherit", stderr: "inherit" })
+const git = (args, opts) => {
+  return execa("git", args, opts);
 };
 
 async function main(cli) {
-  const hash = await git(["rev-parse", "--short", "HEAD"], {cwd: process.cwd()});
-  const cwd = cli.input[0];
+  const {stdout: hash} = await git(["rev-parse", "--short", "HEAD"], {cwd: process.cwd()});
+  const cwd = cli.flags.source;
+  const target = cli.flags.target;
 
   if (!cwd) {
-    console.log("Directory to deploy is required.");
+    console.log("--source directory deploy is required.");
     return cli.help(1);
   }
 
-  await git(["init"], {cwd});
-  git(["remote", "add", "target"], {cwd});
-  git(["commit", "-m", `Deploy "${hash}"`])
+  if (!target) {
+    console.log("--target git url is required.");
+    return cli.help(1);
+  }
+
+  await sander.rimraf(cwd, ".git");
+  await git(["init"], {cwd, stdout: "inherit", stderr: "inherit"});
+  await git(["remote", "add", "target", target], {cwd, stdout: "inherit", stderr: "inherit"});
+  await git(["add", "."], {cwd, stdout: "inherit", stderr: "inherit"});
+  await git(["commit", "-m", `Deploy "${hash}" at ${new Date()}`], {cwd, stdout: "inherit", stderr: "inherit"});
+  await git(["push", "-f", "--set-upstream", "target", "master"], {cwd, stdout: "inherit", stderr: "inherit"});
 }
 
 main(meow(`
   Options
-    --remote git remote url to push to
+    --source    - path to source folder to upload
+    --target    - git remote url to push to
 `));
