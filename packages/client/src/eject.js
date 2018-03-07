6d9e6349a88bf9dacaad587118468729a4037c60
Mario Nebl <hello@mario-nebl.de>
2018-03-08 00:25:25 +0100

    fix: resolve bundles correctly

--- packages/client/src/eject.js
+++ packages/client/src/eject.js
@@ -6,11 +6,11 @@ function eject() {
   const fs = new MemoryFileSystem();
 
   fs.mkdirpSync("/static");
-  fs.writeFileSync("/static/client.js", require("raw-loader!../lib/static/client.js"));
-  fs.writeFileSync("/static/client.js.map", require("raw-loader!../lib/static/client.js.map"));
+  fs.writeFileSync("/static/client.js", require("raw-loader!./static/client.js"));
+  fs.writeFileSync("/static/client.js.map", require("raw-loader!./static/client.js.map"));
 
-  fs.writeFileSync("/static/vendors.js", require("raw-loader!../lib/static/vendors.js"));
-  fs.writeFileSync("/static/vendors.js.map", require("raw-loader!../lib/static/vendors.js.map"));
+  fs.writeFileSync("/static/vendors.js", require("raw-loader!./static/vendors.js"));
+  fs.writeFileSync("/static/vendors.js.map", require("raw-loader!./static/vendors.js.map"));
 
   return fs;
 }
