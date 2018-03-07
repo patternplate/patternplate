cb1bf5095f100317abaf316eda387a402616b28e
frederikreiss <fr@f-reiss.de>
2018-03-07 11:17:02 +0100

    fix: avoid state changes in LabelLink (#162)

--- packages/client/src/app/containers/navigation.js
+++ packages/client/src/app/containers/navigation.js
@@ -5,6 +5,7 @@ import { push } from "react-router-redux";
 import { bindActionCreators } from "redux";
 import { MainNavigation } from "@patternplate/components";
 import { Link } from "@patternplate/components";
+import { patchLocation } from "../actions";
 
 import selectNavigation from "../selectors/navigation";
 import selectDocs from "../selectors/docs";
@@ -34,6 +35,9 @@ function mapDispatch(dispatch) {
 
         const parsed = url.parse(e.currentTarget.href);
         return push(`${parsed.pathname}?${parsed.query}`);
+      },
+      onLabelClick(query) {
+        return patchLocation({query});
       }
     },
     dispatch
