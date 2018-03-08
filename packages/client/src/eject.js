d1a486775b9ea057fe5762dd8c79b3fdfd02250e
Mario Nebl <hello@mario-nebl.de>
2018-03-08 16:31:14 +0100

    refactor: use webpack context for better stability

--- packages/client/src/eject.js
+++ packages/client/src/eject.js
@@ -1,11 +1,17 @@
+const path = require("path");
 const MemoryFileSystem = require("memory-fs");
 
 module.exports = eject;
 
+const ctx = require.context("raw-loader!./static", true, /.js$/);
+
 function eject() {
   const fs = new MemoryFileSystem();
   fs.mkdirpSync("/static");
-  fs.writeFileSync("/static/client.js", require("raw-loader!./static/client.js"));
-  fs.writeFileSync("/static/vendors.js", require("raw-loader!./static/vendors.js"));
+
+  ctx.keys().forEach(key => {
+    fs.writeFileSync(path.join("/static", key), ctx(key));
+  });
+
   return fs;
 }
