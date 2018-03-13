3ce2167d7318b0334ebc597aada079c216c4905f
Mario Nebl <hello@mario-nebl.de>
2018-03-13 18:06:11 +0100

    chore: refine ssh-agent handling

--- packages/deploy/deploy.js
+++ packages/deploy/deploy.js
@@ -43,8 +43,7 @@ async function main(cli) {
 
   if (identity) {
     const {stdout: startAgent} = await execa("ssh-agent", ["-s"]);
-    await execa.shell(startAgent, {stderr: "inherit"});
-    await execa("ssh-add", [identity], {cwd, stderr: "inherit"});
+    await execa.shell(`eval "${startAgent}" && ssh-add ${identity}`, {stdout: "inherit", stdin: "inherit"});
   }
 
   await git(["push", "-f", "--set-upstream", "target", "master"], {cwd, stderr: "inherit"});
