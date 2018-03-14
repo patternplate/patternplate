017645aa7caab66cc88d738322e6db66d406dccb
Mario Nebl <hello@mario-nebl.de>
2018-03-14 14:47:41 +0100

    chore: actually exit for skip conditions

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,10 +4,12 @@ set -u
 
 if [ "$TRAVIS_SECURE_ENV_VARS" != "true" ]; then
     echo "Skipping deployment, secure env vars not available";
+    exit 0;
 fi
 
 if [ "$TRAVIS_BRANCH" != "master" ]; then
     echo "Skipping deployment, not on master";
+    exit 0;
 fi
 
 patternplate-deploy --source .tmp \
