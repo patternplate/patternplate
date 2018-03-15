419f365269dc7cfddb2f84107dcaaff95b4d10c9
Mario Nebl <hello@mario-nebl.de>
2018-03-15 18:04:02 +0100

    chore: fix deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 set -u
 
-BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi
+BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi);
 
 if [ "$TRAVIS_SECURE_ENV_VARS" != "true" ]; then
     echo "Skipping deployment, secure env vars not available";
