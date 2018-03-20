a9b322d4bf5c1d6381a253f2279ea6b14ddf3d00
Mario Nebl <hello@mario-nebl.de>
2018-03-20 15:43:00 +0100

    fix: handle worker init in bundled mode correctly #190

--- packages/api/src/compiler.js
+++ packages/api/src/compiler.js
@@ -1,26 +1,45 @@
-// const path = require("path");
-// const compiler = require("@patternplate/compiler");
+const os = require("os");
+const fs = require("fs");
+const path = require("path");
 const ARSON = require("arson");
 const {fork} = require("child_process");
 const dargs = require("dargs");
 const Observable = require("zen-observable");
 const MemoryFilesystem = require("memory-fs");
+const crypto = require("crypto");
+const debug = require("util").debuglog("PATTERNPLATE");
+const resolvePkg = require("resolve-pkg");
+
+const OPTS = { stdio: ["inherit", "inherit", "inherit", "ipc"] };
 
 module.exports = createCompiler;
 
-const debug = require("util").debuglog("PATTERNPLATE");
-
-const WORKER = require.resolve("./compiler-worker");
-const OPTS = { stdio: ["inherit", "inherit", "inherit", "ipc"] };
-
 async function createCompiler({ cwd, target = "" }) {
-  const worker = fork(WORKER, dargs({cwd, target}), OPTS);
+  const workerSource = process.env.BUNDLE === "@patternplate/cli"
+    ? require("raw-loader!./compiler-worker")
+    : fs.readFileSync(path.join(__dirname, "compiler-worker.js"));
+
+  const hash = crypto.createHash("sha256").update(workerSource).digest("hex");
+
+  const workerPath = process.env.BUNDLE === "@patternplate/cli"
+    ? path.join(resolvePkg(process.env.BUNDLE), `${hash}.js`)
+    : path.join(__dirname, `${hash}.js`);
+
+  fs.writeFileSync(workerPath, workerSource);
+
+  debug(`starting compiler worker at ${workerPath}`);
+
+  const worker = fork(workerPath, dargs({cwd, target}), OPTS);
 
   let watching = false;
 
   const queue = [];
   let listeners = [];
   const next = (message) => listeners.forEach(listener => listener.next(message));
+
+  worker.on("error", err => {
+    console.error(err);
+  });
 
   worker.on("message", envelope => {
     const {type, target, payload} = ARSON.parse(envelope);
