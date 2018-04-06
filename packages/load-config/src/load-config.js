1a250ab793c2f08fd47d1c7632fb64d2c1c334b0
Mario Nebl <hello@mario-nebl.de>
2018-04-06 07:37:57 +0200

    fix: ensure render-default can be resolve from load-config in any case

--- packages/load-config/src/load-config.js
+++ packages/load-config/src/load-config.js
@@ -1,10 +1,9 @@
 const cosmiconfig = require("cosmiconfig");
+const resolveFrom = require("resolve-from");
 
 const DEFAULTS = {
   docs: ["docs/**/*.md", "README.md"],
-  entry: ["lib/**/demo.js"],
-  render: "@patternplate/render-default/render",
-  mount: "@patternplate/render-default/mount"
+  entry: ["lib/**/demo.js"]
 };
 
 module.exports = async ({ cwd }) => {
@@ -12,8 +11,11 @@ module.exports = async ({ cwd }) => {
   const config = await explorer.load(cwd);
 
   if (!config) {
+    const render = resolveFrom.silent(cwd, "@patternplate/render-default/render") || require.resolve("@patternplate/render-default/render");
+    const mount = resolveFrom.silent(cwd, "@patternplate/render-default/mount") || require.resolve("@patternplate/render-default/mount");;
+
     return {
-      config: DEFAULTS,
+      config: {...DEFAULTS, render, mount},
       filepath: null
     };
   }
