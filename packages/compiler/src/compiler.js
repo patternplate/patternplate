4e0c0c875dda91cdf444a3141b61350fb8dfb385
Mario Nebl <hello@mario-nebl.de>
2018-04-13 21:31:16 +0200

    feat: produce helpful error messages for render/mount misconfiguration #208

--- packages/compiler/src/compiler.js
+++ packages/compiler/src/compiler.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const path = require("path");
 const loadConfig = require("@patternplate/load-config");
 const webpackEntry = require("@patternplate/webpack-entry");
@@ -5,6 +6,7 @@ const MemoryFS = require("memory-fs");
 const resolveFrom = require("resolve-from");
 const webpack = require("webpack");
 const nodeExternals = require("webpack-node-externals");
+const readPkg = require("read-pkg");
 
 module.exports = compiler;
 
@@ -24,8 +26,8 @@ async function compiler(options) {
   const entry = { components };
   const bases = [cwd, process.cwd()].filter(Boolean);
 
-  const render = cascadeResolve(config.render, {bases});
-  const mount = cascadeResolve(config.mount, {bases});
+  const render = cascadeResolve(config.render, {bases, cwd});
+  const mount = cascadeResolve(config.mount, {bases, cwd});
 
   if (options.target === "node") {
     entry.render = render;
@@ -39,7 +41,7 @@ async function compiler(options) {
   }
 
   if (typeof config.cover === "string") {
-    entry.cover = cascadeResolve(config.cover, {bases});
+    entry.cover = cascadeResolve(config.cover, {bases, cwd});
   }
 
   const compiler = webpack({
@@ -70,7 +72,7 @@ async function compiler(options) {
   return compiler;
 }
 
-function cascadeResolve(id, {bases}) {
+function cascadeResolve(id, {bases, cwd}) {
   const result = bases.reduce((resolved, base) => {
     if (resolved) {
       return resolved;
@@ -79,8 +81,44 @@ function cascadeResolve(id, {bases}) {
   }, '');
 
   if (typeof result !== "string") {
-    throw new Error(`Could not resolve "${id}" from ${bases.join(", ")}`);
+    // Relative require path
+    if (id.charAt(0) === "." || id.charAt(0) === "/") {
+      const pathBases = bases.map(base => path.join(base, id));
+      throw new Error(`Relatively required file "${id}" does not exist at ${pathBases.join(", ")}`);
+    }
+
+    // Global require path
+    const pkg = getPkg(cwd) || {};
+    const deps = Object.keys(pkg.dependencies || {});
+    const devDeps = Object.keys(pkg.devDependencies || {});
+
+    if (deps.indexOf(id) === -1 && devDeps.indexOf(id) === -1 && !fs.existsSync(path.join(cwd, "lerna.json"))) {
+      throw new Error(`"${id}" is not installed as dependency at ${cwd}/package.json. Please make sure to install it via npm.`);
+    }
+
+    const fragments = id.split("/");
+    const pkgFragments = id.charAt(0) === "@"
+      ? fragments.slice(0, 2)
+      : fragments.slice(0, 1);
+
+    const pkgId = pkgFragments.join("/");
+    const pkgManifest = resolveFrom.silent(process.cwd(), `${pkgId}/package`);
+
+    if (pkgManifest) {
+      const pkg = require(pkgManifest);
+      throw new Error(`"${pkgId}" can be resolved, but "${id.replace(pkgId, '')}" is not available, it might be corrupted.\nPlease reinstall your node_modules and file an issue at ${pkg.bugs.url} if the problem persists.`);
+    } else {
+      throw new Error(`Could not resolve "${id}" from ${bases.join(", ")}`);
+    }
   }
 
   return result;
+}
+
+const getPkg = (...args) => {
+  try {
+    return readPkg.sync(...args);
+  } catch (err) {
+    return null;
+  }
 }
