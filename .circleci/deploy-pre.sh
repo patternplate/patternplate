f9d0e23d3e6aa5b4eb8c155e35687af6e993292d
Mario Nebl <hello@mario-nebl.de>
2018-11-30 19:46:33 +0100

    chore: fix deploy script

--- .circleci/deploy-pre.sh
+++ .circleci/deploy-pre.sh
@@ -3,4 +3,4 @@ set -e
 
 ID=$(if [[ -z "$CIRCLE_PR_NUMBER" ]]; then echo "commit-$CIRCLE_SHA1"; else echo "pr-$CIRCLE_PR_NUMBER"; fi);
 
-patternplate-deploy --source .tmp --name="$CIRCLE_PROJECT_REPONAME-$ID" --provider=surge
+yarn patternplate-deploy --source .tmp --name="$CIRCLE_PROJECT_REPONAME-$ID" --provider=surge
